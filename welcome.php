4351e66387e71eddf5769054499e45d0a36caf6a
Aroonav Mishra <aroonav11@gmail.com>
2016-02-14 10:13:49 +0530

    It is now deployable on the server.

--- welcome.php
+++ welcome.php
@@ -5,11 +5,14 @@ if(isset($_POST['code']) && isset($_POST['file_name']))
 	$data = $_POST['code'];
     $filename = $time . "_" . $_POST['file_name'];
     $ret = file_put_contents('/home/epsilon/swig/code_data/' . $filename, $data, FILE_APPEND);
-    if($ret === false) {
+    if($ret === false)
+    {
         die('There was an error writing this file');
     }
-    else {
-        echo "$ret bytes written to file";
+    else
+    {
+        echo "$ret bytes written to file.";
+        header('Location: complete.html');
     }
 }
 else
